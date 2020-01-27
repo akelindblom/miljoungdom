f07988e6bf9161d16558c19e77bd1d21a0dea944
Norakso <58669649+Norakso@users.noreply.github.com>
2020-01-27 20:13:29 +0100

    Update sida2.php

--- miljoungdom/sida2.php
+++ miljoungdom/sida2.php
@@ -3,33 +3,37 @@
     
     <?php include 'includes/header.php'; ?>
     
-        <div class="header">
-            <div class="headerContent">
-                
-                <h2>Bli medlem</h2>
-                
-                <p>
-                    Miljöungdom är ett förbund av föreningar. Det betyder att en enskild person inte kan gå med direkt i förbundet Miljöungdom utan bara genom att först vara medlem i en miljöförening, och sedan ansluta den föreningen till Miljöungdom.
-                    
-                <br><br>
-                    
-                Om du är medlem i en miljöförening vart som helst i Sverige kan du anmäla din förening att bli en del av förbundet Miljöungdom. En medlemsförening får tillgång till allting som Miljöungdom erbjuder; en gemenskap av likasinnade, kontakter inför framtiden, tips om verksamheten, hjälp 
-                med det juridiska, och inflytande över förbundet Miljöungdom.
-                
-                <br><br>
-                    
-                En förenings medlemskap i miljöungdom är helt gratis och innebär inga åtagande från föreningens sida.
-                
-                <br><br>
-                    
-                Är du inte medlem i en miljöförening, och har ingen lokal miljöförening som du kan gå med i? Miljöungdom erbjuder hjälp med att starta en egen miljöförening. Tryck här för att komma till vår sida om hur du startar en egen miljöförening, det är hur enkelt som helst!
-                </p>
-                
-            </div>
+    <div class="header">
+         <div class="headerContent">                
+            <h2>Bli medlem</h2>
         </div>
+    </div>
+    
+    <div class="section">
+                
+        <h2>Bli medlem</h2>
+
+        <p>
+        Miljöungdom är ett förbund av föreningar. Det betyder att en enskild person inte kan gå med direkt i förbundet Miljöungdom utan bara genom att först vara medlem i en miljöförening, och sedan ansluta den föreningen till Miljöungdom.
+
+        <br><br>
+
+        Om du är medlem i en miljöförening vart som helst i Sverige kan du anmäla din förening att bli en del av förbundet Miljöungdom. En medlemsförening får tillgång till allting som Miljöungdom erbjuder; en gemenskap av likasinnade, kontakter inför framtiden, tips om verksamheten, hjälp 
+        med det juridiska, och inflytande över förbundet Miljöungdom.
+
+        <br><br>
+
+        En förenings medlemskap i miljöungdom är helt gratis och innebär inga åtagande från föreningens sida.
+
+        <br><br>
+
+        Är du inte medlem i en miljöförening, och har ingen lokal miljöförening som du kan gå med i? Miljöungdom erbjuder hjälp med att starta en egen miljöförening. Tryck här för att komma till vår sida om hur du startar en egen miljöförening, det är hur enkelt som helst!
+        </p>
         
-        <!-- JavaScript -->
-        <script type="text/javascript" src="js/script.js"></script>
+    </div>
+        
+    <!-- JavaScript -->
+    <script type="text/javascript" src="js/script.js"></script>
         
     <?php include 'includes/footer.php'; ?>
 </html>
