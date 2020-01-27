8707b4259d43baea2ce3dd57eeaefd5e1d0172eb
Norakso <58669649+Norakso@users.noreply.github.com>
2020-01-27 20:14:28 +0100

    Update sida3.php

--- miljoungdom/sida3.php
+++ miljoungdom/sida3.php
@@ -2,22 +2,26 @@
 <html>
     <?php include 'includes/header.php'; ?>
             
-        <div class="header">
-             <div class="headerContent">
-                
-                <h2>Rubrik här</h2>
-                 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    
-                <br><br>
-                    
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                 
-            </div>
+    <div class="header">
+        <div class="headerContent">                
+            <h2>Nyheter</h2>
         </div>
+    </div>
+    
+    <div class="section">
+
+        <h2>Rubrik här</h2>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+        <br><br>
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    </div>
         
-        <!-- JavaScript -->
-        <script type="text/javascript" src="js/script.js"></script>
+    <!-- JavaScript -->
+    <script type="text/javascript" src="js/script.js"></script>
         
     <?php include 'includes/footer.php'; ?>
 </html>
